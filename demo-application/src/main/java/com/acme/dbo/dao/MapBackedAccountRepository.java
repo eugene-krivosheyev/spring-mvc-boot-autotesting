b175e985291b58da71f9f7516d888c6d12e90e6a
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-07-19 22:54:48 +0300

    Demo application update: Add logging.

--- demo-application/src/main/java/com/acme/dbo/dao/MapBackedAccountRepository.java
+++ demo-application/src/main/java/com/acme/dbo/dao/MapBackedAccountRepository.java
@@ -1,6 +1,8 @@
 package com.acme.dbo.dao;
 
 import com.acme.dbo.domain.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -8,10 +10,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapBackedAccountRepository implements AccountRepository {
+    private static final Logger log = LoggerFactory.getLogger(MapBackedAccountRepository.class);
     private final Map<Integer, Account> accounts ;
 
     public MapBackedAccountRepository(int initialCapacity) {
         accounts = new HashMap<>(initialCapacity);
+        log.debug("Created MapBackedAccountRepository with initial capacity {}", initialCapacity);
     }
 
     @Override
