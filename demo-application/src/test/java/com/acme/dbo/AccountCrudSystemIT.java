bc8330ae07e2d435fa27404ea1f98112afdfc634
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-07-19 23:45:00 +0300

    Demo application update: Refactor test itself to DI subject.

--- demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
+++ demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
@@ -2,11 +2,9 @@ package com.acme.dbo;
 
 import com.acme.dbo.config.Config;
 import com.acme.dbo.controller.AccountController;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,13 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = Config.class)
 public class AccountCrudSystemIT {
-    private AccountController accountController;
-    @Autowired ApplicationContext context;
-
-    @BeforeEach
-    public void initApplicationContext() {
-        accountController = context.getBean(AccountController.class);
-    }
+    @Autowired private AccountController accountController;
 
     @Test
     public void shouldGetNoAccountsWhenNoCreated() {
