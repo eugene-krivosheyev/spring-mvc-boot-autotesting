b9f4f40454ef525bb46d8a055c805c55dcd934da
Yurii Khalpin <yurii@khalpin.ru>
2021-07-27 14:04:45 +0300

    Practice_room_1

--- demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
+++ demo-application/src/test/java/com/acme/dbo/AccountCrudSystemIT.java
@@ -2,13 +2,18 @@ package com.acme.dbo;
 
 import com.acme.dbo.config.Config;
 import com.acme.dbo.controller.AccountController;
+import com.acme.dbo.controller.AccountNotFoundException;
+import com.acme.dbo.domain.Account;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * see {@link org.springframework.test.context.junit.jupiter.SpringJUnitConfig} annotation
@@ -22,4 +27,25 @@ public class AccountCrudSystemIT {
     public void shouldGetNoAccountsWhenNoCreated() {
         assertTrue(accountController.findAll().isEmpty());
     }
+
+    @Test
+    public void shouldGetAccountWhenCreated(){
+        Account dummyAccount = new Account(1, new BigDecimal(1));
+        accountController.create(dummyAccount);
+        assertFalse(accountController.findAll().isEmpty());
+    }
+
+    @Test
+    public void shouldThrowsExceptionWhenAccountNull() throws AccountNotFoundException {
+        Integer dummyId = 1;
+        assertThrows(AccountNotFoundException.class, (Executable) accountController.findById(dummyId));
+    }
+
+    @Test
+    public void shouldGetAccountByIdWhenCreated() throws AccountNotFoundException {
+        Account dummyAccount = new Account(1, new BigDecimal(1));
+        accountController.create(dummyAccount);
+        assertEquals(dummyAccount, accountController.findById(dummyAccount.getId()));
+    }
+
 }
